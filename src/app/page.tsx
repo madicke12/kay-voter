9c892d3ec9f27a3683adae583a9dc099be2da4a0
madicke12 <cissemadicke8@gmail.com>
2024-05-08 22:00:38 +0000

    completed the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,29 @@
 import Image from "next/image";
-import l from '../../assets/Image2560.png' 
+import l from '../../assets/Image2560.png'
 import i from '../../assets/Image 9.png'
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 export default function Home() {
   return (
-    <main className=" w-full bg-yamba h-full p-10">
-      <nav className="flex mr-3 mt-10 text-4xl"> <Image className="mr-3 "  height={20}  alt="" width={25} src={l}/>KayVoter</nav>
-      <div className="flex item">
+    <main className=" w-full yamba h-screen p-16 text-white">
+      <nav className="flex mr-3  text-4xl "> <Image className="mr-3 " height={20} alt="" width={25} src={l} />KayVoter</nav>
+      <div className="flex  mt-10">
         <div className="flex items-center  justify-center">
-        <div >
-          <span className=" text-[45px] w-24 mt-10 mr-10">
-            Bienvenu sur votre platforme en ligne
-          </span>
-        </div>
-        <Image src={i}  alt="" height={400} width={400}/>
+          <div className="mr-52">
+            <span className=" text-[45px]  mt-10 ">
+              Bienvenu sur votre <br />platforme en ligne<br />
+              Simple ,Rapide et Efficace
+            </span>
+            <div className="mt-8">
+            <span className=" font-mono font-bold text-xl">
+              Ceci est un dispositif de vote en ligne qui vous permet <br/>de voter en toute sécurité 
+              et en toute transparence
+            </span>
+            </div>
+            
+            <Button className="mt-10 bg-main hover:bg-cyan-400 w-[250px]" asChild ><Link href={'/login'}> Commencer</Link></Button>
+          </div>
+          <Image src={i} alt="" height={400} width={400} />
         </div>
       </div>
     </main>
