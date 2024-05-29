1fe71acaa8dbd298bc78d83cc6365fa5ba6a7eeb
madicke12 <cissemadicke8@gmail.com>
2024-05-29 21:34:56 +0000

    come conf

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
     images: {
         dangerouslyAllowSVG: true,
-        domains: ['cdn.builder.io','builder.io'],
+        domains: ['cdn.builder.io','builder.io','app.visily.ai'],
     },
 };
 
