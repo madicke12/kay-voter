cd16239d1529a74584945c08e14157ddc987dfba
madicke12 <cissemadicke8@gmail.com>
2024-06-27 16:06:35 +0000

    walada

--- components/parti.tsx
+++ components/parti.tsx
@@ -1,22 +1,21 @@
 import Image from "next/image";
 
 type PartyProps = {
-    name: string;
-    date: string;
-    iconSrc: string;
-    logoSrc: string;
-    altText: string;
+    nom: string;
+    dateCreation: string;
   };
   
-  const Party: React.FC<PartyProps> = ({ name, date, iconSrc, logoSrc, altText }) => (
+   const iconSrc ='https://cdn.builder.io/api/v1/image/assets/TEMP/f8ed98abb243c607b6195f9272542f89eb7b4f3b31279cb19408e6963b88d944?apiKey=9c066bb72ce5442ca7b521d698a43bb1&'
+  const logoSrc = 'https://cdn.builder.io/api/v1/image/assets/TEMP/b4c333d3be07fa25d009b4ccfc73b9658c6db0624655f3d8b5a0e8a47ae0f3f2?apiKey=9c066bb72ce5442ca7b521d698a43bb1&'
+  const Party: React.FC<PartyProps> = ({ nom, dateCreation, }) => (
     <div className="flex gap-0 items-center justify-between px-5 w-full bg-black bg-opacity-0 max-md:flex-wrap border border-t-1 max-md:max-w-full">
       <div className="justify-center items-start px-5 py-7 font-bold text-zinc-900">
-        {name}
+        {nom}
       </div>
       <div className="flex gap-1">
       <div className="flex gap-1 justify-center    text-zinc-400">
-        <Image width={100} height={100} loading="lazy" src={iconSrc} alt={altText} className="shrink-0 w-4 aspect-square" />
-        <div className="flex-auto my-auto">{date}</div>
+        <Image width={100} height={100} loading="lazy" src={iconSrc} alt={''} className="shrink-0 w-4 aspect-square" />
+        <div className="flex-auto my-auto">{dateCreation}</div>
       </div>
       <Image width={100} height={100} loading="lazy" src={logoSrc} alt=""/>
       </div>
