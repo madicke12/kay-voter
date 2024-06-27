edc4259b0277327946be09f7f2cd03b9598b4fcb
madicke12 <cissemadicke8@gmail.com>
2024-06-27 16:06:40 +0000

    balada

--- components/slide.tsx
+++ components/slide.tsx
@@ -1,51 +1,33 @@
 
 import { Card, CardContent } from "@/components/ui/card"
 import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
+       Carousel,
+       CarouselContent,
+       CarouselItem,
+       CarouselNext,
+       CarouselPrevious,
 } from "@/components/ui/carousel"
 import CandidateDialog from "./candidateDialog"
+import { getElectionCandidates , getAvalaibleElection } from "../server/action"
 
-export function CarouselSize() {
-  return (
-    <Carousel
-      opts={{
-        align: "start",
-      }}
-      className="w-[1000px] max-w-sm"
-    >
-      <CarouselContent>
-       
-          <CarouselItem  >
-                 <CandidateDialog/>
-          </CarouselItem>
-          <CarouselItem  >
-                 <CandidateDialog/>
-          </CarouselItem>
+export async function CarouselSize() {
+       const availableElection = await getAvalaibleElection() ;
+       const candidates = await getElectionCandidates(availableElection.id) ;
 
-          <CarouselItem  >
-                 <CandidateDialog/>
-          </CarouselItem>
-          <CarouselItem  >
-                 <CandidateDialog/>
-          </CarouselItem>
-          <CarouselItem  >
-                 <CandidateDialog/>
-          </CarouselItem>
-          <CarouselItem  >
-                 <CandidateDialog/>
-          </CarouselItem>
-          <CarouselItem  >
-                 <CandidateDialog/>
-          </CarouselItem>
-
-
-      </CarouselContent>
-      <CarouselPrevious />
-      <CarouselNext />
-    </Carousel>
-  )
+       return (
+              <Carousel
+                     opts={{
+                            align: "start",
+                     }}
+                     className="w-[1000px] max-w-sm"
+              >
+                     <CarouselContent>
+                            {candidates.map(item=>  <CarouselItem key={item.id}  >
+                                   <CandidateDialog nom={item.nom} prenom={item.prenom} photoUrl={item.photoUrl} electionId={item.electionId} candidatId={item.id} />
+                            </CarouselItem>)}
+                     </CarouselContent>
+                     <CarouselPrevious />
+                     <CarouselNext />
+              </Carousel>
+       )
 }
